0d79da8c9230fc8661906d4ce8f8df3d801076ad
yc <yc@cht.com.tw>
2024-02-27 10:15:33 +0800

    增加繁體中文支援

--- cn2an/transform.py
+++ cn2an/transform.py
@@ -2,19 +2,31 @@ import re
 
 from .cn2an import Cn2An
 from .an2cn import An2Cn
-from .conf import UNIT_CN2AN
+from .conf import UNIT_CN2AN, WORD_TO_SIM, WORD_TO_TRA
 
 
 class Transform(object):
     def __init__(self) -> None:
-        self.all_num = "零一二三四五六七八九"
+        self.all_num_ezero = "一二两三四五六七八九"
+        self.all_num = "零一二两三四五六七八九"
         self.all_unit = "".join(list(UNIT_CN2AN.keys()))
         self.cn2an = Cn2An().cn2an
         self.an2cn = An2Cn().an2cn
+        self.WORD_TO_SIM = WORD_TO_SIM
+        self.WORD_TO_TRA = WORD_TO_TRA
+        self.cn_zeropattern = f"负?([零{self.all_num}]+点)?[{self.all_num}]+|负?零[{self.all_num}]+|零[{self.all_num}]+"
+        self.zeroheading = f"负?零+[{self.all_num_ezero}]"
         self.cn_pattern = f"负?([{self.all_num}{self.all_unit}]+点)?[{self.all_num}{self.all_unit}]+"
+        self.cn_pattern_added = f"负?([{self.all_num}{self.all_unit}]+点)?[{self.all_num}{self.all_unit}]+|\
+            负?([{self.all_num}{self.all_unit}{self.all_num}]+点)?[{self.all_num}{self.all_unit}{self.all_num}]+|\
+            负?([{self.all_num}{self.all_unit}{self.all_num}{self.all_unit}]+点)?[{self.all_num}{self.all_unit}{self.all_num}{self.all_unit}]+"
         self.smart_cn_pattern = f"-?([0-9]+.)?[0-9]+[{self.all_unit}]+"
 
     def transform(self, inputs: str, method: str = "cn2an") -> str:
+        #To Sim Chinese
+        for nor_num in self.WORD_TO_SIM.keys():
+            inputs = inputs.replace(nor_num, self.WORD_TO_SIM[nor_num])
+        
         if method == "cn2an":
             inputs = inputs.replace("廿", "二十").replace("半", "0.5").replace("两", "2")
             # date
@@ -31,8 +43,12 @@ class Transform(object):
             inputs = re.sub(fr"{self.cn_pattern}摄氏度",
                             lambda x: self.__sub_util(x.group(), "cn2an", "celsius"), inputs)
             # number
-            output = re.sub(self.cn_pattern,
+            inputs = re.sub(self.cn_pattern_added,
                             lambda x: self.__sub_util(x.group(), "cn2an", "number"), inputs)
+            # zeronumber
+            output = re.sub(self.cn_zeropattern,
+                            lambda x: self.__sub_util(x.group(), "cn2an", "zeronumber"), inputs)
+
 
         elif method == "an2cn":
             # date
@@ -53,6 +69,10 @@ class Transform(object):
         else:
             raise ValueError(f"error method: {method}, only support 'cn2an' and 'an2cn'!")
 
+        #Back to Tra Chinese
+        for nor_num in self.WORD_TO_TRA.keys():
+            output = output.replace(nor_num, self.WORD_TO_TRA[nor_num])
+        print (str(input))
         return output
 
     def __sub_util(self, inputs, method: str = "cn2an", sub_mode: str = "number") -> str:
@@ -77,7 +97,17 @@ class Transform(object):
                         return re.sub(f"{self.cn_pattern}(?=摄氏度)",
                                       lambda x: str(self.cn2an(x.group(), "smart")), inputs).replace("摄氏度", "℃")
                     elif sub_mode == "number":
-                        return str(self.cn2an(inputs, "smart"))
+                        if inputs[0] != "零" and inputs[:2] != "负零":
+                            return str(self.cn2an(inputs, "smart"))
+                        else:
+                            return inputs
+                    elif sub_mode == "zeronumber":
+                        try:
+                            rpl_str = re.match(f"{self.zeroheading}", inputs).group(0)[:-1]
+                        except:
+                            rpl_str = ""
+                        inputs = inputs.replace("负","")
+                        return str(rpl_str.replace("负","-").replace("零","0"))+str(self.cn2an(inputs, "smart"))
                     else:
                         raise Exception(f"error sub_mode: {sub_mode} !")
                 else:
